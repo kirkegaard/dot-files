83ba715bf5868da2bec49c924bdb4b465169bf89
AUTHORNAME <AUTHOREMAIL>
2013-06-12 20:41:13 +0200

    bump

--- bin/nowplaying.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/bash
-arch -i386 osascript -e 'tell application "System Events" to if ((name of processes) contains "iTunes") then do shell script ("osascript -e " & quoted form of ("tell application \"iTunes\" to if player state is playing then \"Now Playing: \" & name of current track & \" - \" & artist of current track" & ""))'
